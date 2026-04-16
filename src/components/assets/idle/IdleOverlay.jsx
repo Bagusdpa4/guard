abddd97f485dcd162b374ca6ba46c18af5416975
Bagus Dwi <sugab.dwi88@gmail.com>
2026-04-16 14:38:26 +0700

    Fixing Video

--- src/components/assets/idle/IdleOverlay.jsx
+++ src/components/assets/idle/IdleOverlay.jsx
@@ -70,7 +70,7 @@ export const IdleOverlay = () => {
             className="absolute inset-0 h-full w-full object-cover opacity-80"
           >
             {/* Path langsung ke root jika file ada di /public/Intro.mp4 */}
-            <source src="/1080x1920.mp4" type="video/mp4" />
+            <source src="/Shelter Guard.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
 
